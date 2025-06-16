d19adcd38ecb8cda35ddfab17b5bf084e090af32
sarojdhakal307 <sarojdhakal307@gmail.com>
2025-06-16 16:33:27 +0545

    added registeredclz on landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,12 @@
 import Adsection from '@/components/adsection';
 import HeroSection from '@/components/hero';
+import RegisteredClzList from '@/components/registeredclzlist';
 export default function Home() {
   return (
     <>
     <HeroSection/>
     <Adsection adsList ={["/adsfiles/texasclz.gif","/adsfiles/thamesclz.gif","/adsfiles/careerconclave.png"]}/>
+    <RegisteredClzList />
     </>
   );
 }
